7ddca2e5a92dc0d4c67d44d067686256187b117b
VarshaPatel99 <kumarivarsharani2002@gmail.com>
2026-05-29 21:46:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // Rectangle Class
+// Rectangle Class
 class Rectangle {
   constructor(width, height) {
     this._width = width;
@@ -16,7 +17,7 @@ class Rectangle {
   }
 
   // Method to calculate area
-  calculateArea() {
+  getArea() {
     return this._width * this._height;
   }
 }
@@ -24,28 +25,11 @@ class Rectangle {
 // Square Class inherits from Rectangle
 class Square extends Rectangle {
   constructor(side) {
-    // width and height are equal in a square
     super(side, side);
   }
 
   // Method to calculate perimeter
-  calculatePerimeter() {
+  getPerimeter() {
     return 4 * this.width;
   }
 }
-
-// Create Rectangle object
-const rectangle = new Rectangle(10, 5);
-
-console.log("Rectangle");
-console.log("Width:", rectangle.width);
-console.log("Height:", rectangle.height);
-console.log("Area:", rectangle.calculateArea());
-
-// Create Square object
-const square = new Square(6);
-
-console.log("\nSquare");
-console.log("Side:", square.width);
-console.log("Area:", square.calculateArea());
-console.log("Perimeter:", square.calculatePerimeter());
