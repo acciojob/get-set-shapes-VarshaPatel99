30d0e329d29022a22ae13f88e01603ee280ed6fe
VarshaPatel99 <kumarivarsharani2002@gmail.com>
2026-06-07 10:06:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,39 +1,32 @@
-// Rectangle Class
 class Rectangle {
   constructor(width, height) {
     this._width = width;
     this._height = height;
   }
 
-  // Getter for width
   get width() {
     return this._width;
   }
 
-  // Getter for height
   get height() {
     return this._height;
   }
 
-  // Method to calculate area
   getArea() {
     return this._width * this._height;
   }
 }
 
-// Square Class inherits from Rectangle
 class Square extends Rectangle {
   constructor(side) {
-    // Call Rectangle constructor
     super(side, side);
   }
 
-  // Method to calculate perimeter
   getPerimeter() {
     return this.width * 4;
   }
 }
 
-// Make classes available globally
+/* Do not change the code below */
 window.Rectangle = Rectangle;
 window.Square = Square;
